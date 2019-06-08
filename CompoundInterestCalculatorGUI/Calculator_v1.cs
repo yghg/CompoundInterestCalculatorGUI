b20c84cd0616969a012bada7e3b67feaef4ddf65
yghg <43450199+yghg@users.noreply.github.com>
2019-06-08 11:00:30 -0400

    Add files via upload

---
+++ CompoundInterestCalculatorGUI/Calculator_v1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompoundInterestCalculatorGUI
+{
+    class Calculator_v1
+    {
+        // variables
+        private double compound;
+        public double _compound
+        {
+            get { return compound; }
+            set { compound = value; }
+        }
+
+        private double principal;       
+        public double _principal
+        {
+            get { return principal; }
+            set { principal = value; }
+        }
+
+        private double interest;
+        public double _interest
+        {
+            get { return interest; }
+            set { interest = value; }
+        }
+
+        private int years;
+        public int _years
+        {
+            get { return years; }
+            set { years = value; }
+        }
+
+        // do the math
+        // compound = principal(1 + (interest/n)^(n*years))
+        public double CalculateInterest()
+        {
+            principal = _principal;
+            interest = _interest;
+            years = _years;
+            compound = (principal * Math.Pow((1 + (interest / 1)), (1 * years)));
+            return compound;
+        }
+    }
+}
---
+++ CompoundInterestCalculatorGUI/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CompoundInterestCalculatorGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculator_v1 calc = new Calculator_v1();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private double ValidateValues(string txt)
+        {
+            double value;
+            bool isDouble = Double.TryParse(txt, out value);
+            if (!isDouble)
+            {
+                MessageBox.Show("Please enter valid values and try again.", "Error");
+            }
+            else if (isDouble)
+            {
+                return value = double.Parse(txt);
+            }            
+            return value = 0;       
+        }
+
+        private double ValidateYears(string txt)
+        {
+            int value;
+            bool isInt = int.TryParse(txt, out value);
+            if (!isInt)
+            {
+                MessageBox.Show("Please enter a valid value for 'Years to Compound' and try again.", "Error");
+            }
+            else if (isInt)
+            {
+                return value = int.Parse(txt);
+            }            
+            return value = 0;
+            
+        }
+
+        private void ButtonCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            calc._principal = ValidateValues(textBoxPrincipal.Text);
+            calc._interest = ValidateValues(textBoxInterest.Text);
+            calc._years = (int)ValidateYears(textBoxYears.Text);
+            textBoxResult.Text = calc.CalculateInterest().ToString("C2");
+        }
+    }
+}
